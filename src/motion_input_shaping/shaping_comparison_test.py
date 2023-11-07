3d6ca6393210a21c42a1b44e1331c7a14e3edf76
Jay Leong <jay@id.izaber.com>
2023-11-07 10:12:14 -0800

    Modified shaping_comparison_test.py to test both shapers and added subplot to display full trajectory

--- src/motion_input_shaping/shaping_comparison_test.py
+++ src/motion_input_shaping/shaping_comparison_test.py
@@ -10,10 +10,12 @@ capture position data.
 import time
 import sys
 from matplotlib import pyplot as plt
-from zaber_motion import Units
-from zaber_motion.ascii import Connection
+from zaber_motion import Units, CommandFailedException
+from zaber_motion.ascii import Connection, Axis, Lockstep
 from shaped_axis import ShapedAxis
+from shaped_axis_stream import ShapedAxisStream
 from plant import Plant
+from zero_vibration_stream_generator import ShaperType
 from step_response_data import StepResponseData
 
 # ------------------- Script Settings ----------------------
@@ -25,61 +27,71 @@ MOVE_DISTANCE = 5  # The move distance in mm to test.
 SCOPE_TIMEBASE = 1  # The scope sampling period in ms. Adjust if the whole move isn't captured.
 RESONANT_FREQUENCY = 5.07  # Input shaping resonant frequency in Hz.
 DAMPING_RATIO = 0.1  # Input shaping damping ratio.
+STREAM_SHAPER_TYPE = ShaperType.ZV  # Input shaper type for ShapedAxisStream
+SETTLING_TIME = 1  # Amount of time to wait between moves for vibrations to settle
 
 # ------------------- Script Settings ----------------------
 
 
-def plot(data_unshaped: StepResponseData, data_shaped: StepResponseData) -> None:
+def plot(datasets: list[StepResponseData], labels: list[str], colors: list[str]) -> None:
     """
-    Plot the shaped and unshaped step response.
+    Plot step response data.
 
-    :param data_unshaped: The unshaped step response data
-    :param data_shaped: The shaped step response data
+    :param datasets: List of step response data to plot
+    :param labels: List of legend labels for each step response data
+    :param colors: List of plot colors for each step response data
     """
-    # Setup the plot
-    plt.figure(1)
-    plt.title("Device Response")
-    plt.xlabel("Time [ms]")
-    plt.ylabel("Position [um]")
-    plt.grid(True)
+    # Set up the plot
+    fig = plt.figure(1)
+
+    # Plot motion trajectory
+    ax_input = fig.add_subplot(2, 1, 1)
+    ax_input.set_title("Input Motion")
+    ax_input.set_xlabel("Time [ms]")
+    ax_input.set_ylabel("Position [um]")
+    ax_input.grid(True)
+
+    for n, data in enumerate(datasets):
+        ax_input.plot(
+            data.get_time_stamps(),
+            data.get_target_positions(False),
+            label=f"Target Position ({labels[n]})",
+            color=colors[n],
+        )
+
+    ax_input.legend(loc="lower right")
+
+    # Plot response zoomed into to see residual vibration
+    ax_response = fig.add_subplot(2, 1, 2)
+    ax_response.set_title("Device Response")
+    ax_response.set_xlabel("Time [ms]")
+    ax_response.set_ylabel("Position [um]")
+    ax_response.grid(True)
 
     # Setup the y axes so only the final approach is shown
-    ylimits_unshaped = data_unshaped.get_trajectory_settling_limits(True)
-    ylimits_shaped = data_shaped.get_trajectory_settling_limits(True)
-    plt.ylim(
-        min(ylimits_shaped[0], ylimits_unshaped[0]), max(ylimits_shaped[1], ylimits_unshaped[1])
-    )
+    ylimits = [x.get_trajectory_settling_limits(True) for x in datasets]
+    ax_response.set_ylim(min(x[0] for x in ylimits), max(x[1] for x in ylimits))
 
-    # Plot both data sets
-    plt.plot(
-        data_unshaped.get_time_stamps(),
-        data_unshaped.get_target_positions(True),
-        label="Target Position (unshaped)",
-        color="black",
-        linestyle="--",
-    )
-    plt.plot(
-        data_unshaped.get_time_stamps(),
-        data_unshaped.get_measured_positions(True),
-        label="Measured Position (unshaped)",
-        color="black",
-    )
+    # Plot datasets
+    for n, data in enumerate(datasets):
+        ax_response.plot(
+            data.get_time_stamps(),
+            data.get_target_positions(True),
+            label=f"Target ({labels[n]})",
+            color=colors[n],
+            linestyle="--",
+        )
+        ax_response.plot(
+            data.get_time_stamps(),
+            data.get_measured_positions(True),
+            label=f"Measured ({labels[n]})",
+            color=colors[n],
+        )
 
-    plt.plot(
-        data_shaped.get_time_stamps(),
-        data_shaped.get_target_positions(True),
-        label="Target Position (shaped)",
-        color="red",
-        linestyle="--",
-    )
-    plt.plot(
-        data_shaped.get_time_stamps(),
-        data_shaped.get_measured_positions(True),
-        label="Measured Position (shaped)",
-        color="red",
-    )
+    ax_response.legend(loc="lower right")
 
-    plt.legend(loc="lower right")
+    fig.tight_layout()
+
     plt.show()  # type: ignore
 
 
@@ -100,15 +112,36 @@ if __name__ == "__main__":
             print(f"Not enough axes for specified axis index ({AXIS_INDEX}), exiting.")
             sys.exit(0)
 
+        # Check if axis is part of lockstep group
+        LOCKSTEP_INDEX = 0
+        try:
+            num_lockstep_groups_possible = device.settings.get("lockstep.numgroups")
+            for group_num in range(1, int(num_lockstep_groups_possible) + 1):
+                axis_nums = device.get_lockstep(group_num).get_axis_numbers()
+                if AXIS_INDEX in axis_nums:
+                    print(f"Axis {AXIS_INDEX} is part of Lockstep group {group_num}.")
+                    LOCKSTEP_INDEX = group_num
+                    break
+        except CommandFailedException:
+            # Unable to get lockstep.numgroups settings meaning device is not capable of lockstep.
+            pass
+
+        zaber_object: Axis | Lockstep
+        if LOCKSTEP_INDEX == 0:
+            zaber_object = device.get_axis(AXIS_INDEX)
+        else:
+            zaber_object = device.get_lockstep(LOCKSTEP_INDEX)
+
         plant = Plant(RESONANT_FREQUENCY, DAMPING_RATIO)
-        shaped_axis = ShapedAxis(device.get_axis(AXIS_INDEX), plant)
+        shaped_axis = ShapedAxis(zaber_object, plant)
+        shaped_axis_stream = ShapedAxisStream(zaber_object, plant, STREAM_SHAPER_TYPE)
 
         # Home the axis and move to zero position
         if not shaped_axis.is_homed():
             shaped_axis.axis.home()
 
         shaped_axis.axis.move_absolute(0, Units.LENGTH_MILLIMETRES, True)
-        time.sleep(0.2)
+        time.sleep(SETTLING_TIME)
 
         # Get the first dataset with no shaping
         print("Performing unshaped move.")
@@ -122,7 +155,7 @@ if __name__ == "__main__":
             True,
         )
 
-        time.sleep(0.5)
+        time.sleep(SETTLING_TIME)
 
         # Get the second dataset with shaping
         print("Performing shaped move.")
@@ -138,6 +171,26 @@ if __name__ == "__main__":
 
         shaped_axis.reset_deceleration()
 
-    plot(step_response_data_unshaped, step_response_data_shaped)
+        time.sleep(SETTLING_TIME)
+
+        # Get the second dataset with shaping
+        print("Performing shaped move with streams.")
+        step_response_data_shaped_stream = StepResponseData(
+            SCOPE_TIMEBASE, Units.TIME_MILLISECONDS, Units.LENGTH_MICROMETRES
+        )
+
+        step_response_data_shaped_stream.capture_data(
+            shaped_axis.axis,
+            lambda: shaped_axis_stream.move_relative(
+                MOVE_DISTANCE, Units.LENGTH_MILLIMETRES, False
+            ),
+            True,
+        )
+
+    plot(
+        [step_response_data_unshaped, step_response_data_shaped, step_response_data_shaped_stream],
+        ["Unshaped", "ShapedAxis", f"ShapedAxisStream ({STREAM_SHAPER_TYPE.name})"],
+        ["black", "red", "blue"],
+    )
 
     print("Complete.")
