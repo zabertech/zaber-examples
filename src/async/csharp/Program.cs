3b98ca358e7bc8b3cd14e116ec986c7bda9e7330
SoleilAtZaber <45833544+SoleilAtZaber@users.noreply.github.com>
2024-01-31 17:40:01 -0800

    Update src/async/csharp/Program.cs
    Co-authored-by: martinzak-zaber <48802908+martinzak-zaber@users.noreply.github.com>

--- src/async/csharp/Program.cs
+++ src/async/csharp/Program.cs
@@ -65,7 +65,7 @@ await using (var connection = await Connection.OpenSerialPortAsync(PORT))
 
     // Home the devices and wait until done.
     // This is an example of overlapping async commands. Order of execution is not
-    // guaranteed and with more than two or three there is a risk of a device error.
+    // guaranteed. With more than two or three tasks there is a risk of exceeding devices communication capacity.
     await Task.WhenAll(xAxis.HomeAsync(), yAxis.HomeAsync());
 
     // Grid scan loop
